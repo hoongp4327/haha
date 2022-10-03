279ba9f451b1c9373ad9fdef54d160d2437e243f
hoàng <34977918+hoongp4327@users.noreply.github.com>
2022-10-04 00:43:19 +0700

    Update js.main.js

--- js.main.js
+++ js.main.js
@@ -5,13 +5,13 @@ setTimeout(function(){
 
 swal({  
 
- title: "Bạn nhận được một món quà !",   
-text: "Thái Hoàng vừa gửi bạn một món quà đặc biệt nhân ngày sinh nhật của bạn ",
-imageUrl: "https://graph.facebook.com/108165012698422/picture?type=large",
+ title: "Bạn nhận được một món quà từ Thái Hoàng !",   
+text: "Tôi gửi bạn một món quà đặc biệt nhân ngày 1 cô bé đần ra đời ",
+imageUrl: "https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-1/299722013_1402027753623099_1874277685974387252_n.jpg?stp=dst-jpg_p240x240&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_ohc=UvaBeSSQiDEAX_B7RxU&_nc_ht=scontent.fhan17-1.fna&oh=00_AT80Ng6xxq-H_0O1xkwyTl0JJVttGBxqaOMdgfJ6TgLWDA&oe=6340E0B8",
   showCancelButton: true,
   confirmButtonColor: "#d3d030", 
   confirmButtonText: "Nhận quà ngay",
-   cancelButtonText: "Đéo cần, cám ơn .", 
+   cancelButtonText: "Không cần đâu", 
   closeOnConfirm: false,  
  closeOnCancel: false }, 
 function(isConfirm){  
@@ -19,8 +19,8 @@ function(isConfirm){
  
 
 swal({  
- title: "Bạn có phải là Thuỷ ? ", 
-  text: "Nhập mã xác nhận nè ",  
+ title: "Bạn có phải là Mai ? ", 
+  text: "Nhập mã xác nhận hoặc liên hệ để nhận mã xác nhận ",  
  type: "input",  
  showCancelButton: true,  
  closeOnConfirm: false,   
@@ -29,28 +29,28 @@ animation: "slide-from-top",
  inputPlaceholder: "Mã xác nhận là gì ?" },
  function(inputValue){   
 if (inputValue === false) return false;   
-   if (inputValue !== "thanhhoa") {   
-  swal.showInputError("Nhập sai rồi kìa má ơi :v ");   
+   if (inputValue !== "em ơi đừng khóc bóng tối trước mắt sẽ bắt em đi") {   
+  swal.showInputError("Nhập sai rồi bà già :) ");   
   return false   }   
  
 setTimeout(function(){
 
 
 swal({  
- title: "Chúc em một tuổi mới", 
-  text: "Bớt xinh, đỗ đại học mình mong muốn, bắn pubg provip, nói chuyện luôn bựa, ngày này là ngày một thiên thần ra đời :D  anh mày định chúc bựa nhưng sợ bị chửi hic ",  
+ title: "Chúc bạn một tuổi mới", 
+  text: "Trap được nhiều chú bé đần, vui vẻ và hạnh phúc bên cạnh gia đình, bạn bè và thằng oắt con, rất xinh đẹp từ trần",  
  type: "input",  
  showCancelButton: true,  
  closeOnConfirm: false,   
 animation: "slide-from-top",  
 
- inputPlaceholder: " ĐM để tiếp tục " },
+ inputPlaceholder: " Quà trong ngăn bàn của Quân, gõ bất kỳ để đi nhận quà " },
  function(inputValue){   
 if (inputValue === false) return false;   
    if (inputValue === "") {   
   swal.showInputError("Không thể để trống được ");   
   return false   }  
- swal("ok ! ", + inputValue + " Chúc em một ngày Sinh Nhật thật Vui Vẻ nhé ^^   ", "success"); });
+ swal("ok ! ", + "OK nè" + " Chúc bạn có một ngày Sinh Nhật thật Vui Vẻ nhé ^^   ", "success"); });
 
 
 },3000);
@@ -61,7 +61,7 @@ if (inputValue === false) return false;
 
  }
 else {   
-  swal("KHÔNG NHẬN ", "Bạn vừa từ chối món quà từ THÁI HOÀNG ,   tải lại trang nếu muốn nhận lại quà :D ", "error"); 
+  swal("KHÔNG NHẬN? ", "Bạn vừa từ chối món quà từ THOÀNG, bạn ý đã dỗi hãy tải lại trang nếu muốn nhận lại quà :) ", "error"); 
 
   } });
 
